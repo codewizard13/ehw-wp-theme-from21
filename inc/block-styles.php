3b97e3455d708c09224754df4f60a643c276ec64
Eric L. Hepperle <codewizard13@gmail.com>
2025-06-09 07:49:25 -0500

    🎉    INITIAL COMMIT --  THEME from21 = Custom WP Theme based on TwentyTwentyOne
    - Changed all instances / references from `twentytwentyone` ->  `from21`

---
+++ inc/block-styles.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Block Styles
+ *
+ * @link https://developer.wordpress.org/reference/functions/register_block_style/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+if ( function_exists( 'register_block_style' ) ) {
+	/**
+	 * Register block styles.
+	 *
+	 * @since Twenty Twenty-One 1.0
+	 *
+	 * @return void
+	 */
+	function twenty_twenty_one_register_block_styles() {
+		// Columns: Overlap.
+		register_block_style(
+			'core/columns',
+			array(
+				'name'  => 'from21-columns-overlap',
+				'label' => esc_html__( 'Overlap', 'from21' ),
+			)
+		);
+
+		// Cover: Borders.
+		register_block_style(
+			'core/cover',
+			array(
+				'name'  => 'from21-border',
+				'label' => esc_html__( 'Borders', 'from21' ),
+			)
+		);
+
+		// Group: Borders.
+		register_block_style(
+			'core/group',
+			array(
+				'name'  => 'from21-border',
+				'label' => esc_html__( 'Borders', 'from21' ),
+			)
+		);
+
+		// Image: Borders.
+		register_block_style(
+			'core/image',
+			array(
+				'name'  => 'from21-border',
+				'label' => esc_html__( 'Borders', 'from21' ),
+			)
+		);
+
+		// Image: Frame.
+		register_block_style(
+			'core/image',
+			array(
+				'name'  => 'from21-image-frame',
+				'label' => esc_html__( 'Frame', 'from21' ),
+			)
+		);
+
+		// Latest Posts: Dividers.
+		register_block_style(
+			'core/latest-posts',
+			array(
+				'name'  => 'from21-latest-posts-dividers',
+				'label' => esc_html__( 'Dividers', 'from21' ),
+			)
+		);
+
+		// Latest Posts: Borders.
+		register_block_style(
+			'core/latest-posts',
+			array(
+				'name'  => 'from21-latest-posts-borders',
+				'label' => esc_html__( 'Borders', 'from21' ),
+			)
+		);
+
+		// Media & Text: Borders.
+		register_block_style(
+			'core/media-text',
+			array(
+				'name'  => 'from21-border',
+				'label' => esc_html__( 'Borders', 'from21' ),
+			)
+		);
+
+		// Separator: Thick.
+		register_block_style(
+			'core/separator',
+			array(
+				'name'  => 'from21-separator-thick',
+				'label' => esc_html__( 'Thick', 'from21' ),
+			)
+		);
+
+		// Social icons: Dark gray color.
+		register_block_style(
+			'core/social-links',
+			array(
+				'name'  => 'from21-social-icons-color',
+				'label' => esc_html__( 'Dark gray', 'from21' ),
+			)
+		);
+	}
+	add_action( 'init', 'twenty_twenty_one_register_block_styles' );
+}
---
+++ single.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+
+/* Start the Loop */
+while ( have_posts() ) :
+	the_post();
+
+	get_template_part( 'template-parts/content/content-single' );
+
+	if ( is_attachment() ) {
+		// Parent post navigation.
+		the_post_navigation(
+			array(
+				/* translators: %s: Parent post link. */
+				'prev_text' => sprintf( __( '<span class="meta-nav">Published in</span><span class="post-title">%s</span>', 'from21' ), '%title' ),
+			)
+		);
+	}
+
+	// If comments are open or there is at least one comment, load up the comment template.
+	if ( comments_open() || get_comments_number() ) {
+		comments_template();
+	}
+
+	// Previous/next post navigation.
+	$from21_next = is_rtl() ? twenty_twenty_one_get_icon_svg( 'ui', 'arrow_left' ) : twenty_twenty_one_get_icon_svg( 'ui', 'arrow_right' );
+	$from21_prev = is_rtl() ? twenty_twenty_one_get_icon_svg( 'ui', 'arrow_right' ) : twenty_twenty_one_get_icon_svg( 'ui', 'arrow_left' );
+
+	$from21_next_label     = esc_html__( 'Next post', 'from21' );
+	$from21_previous_label = esc_html__( 'Previous post', 'from21' );
+
+	the_post_navigation(
+		array(
+			'next_text' => '<p class="meta-nav">' . $from21_next_label . $from21_next . '</p><p class="post-title">%title</p>',
+			'prev_text' => '<p class="meta-nav">' . $from21_prev . $from21_previous_label . '</p><p class="post-title">%title</p>',
+		)
+	);
+endwhile; // End of the loop.
+
+get_footer();
